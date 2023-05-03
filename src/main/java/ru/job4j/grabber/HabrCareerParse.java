faf591f8b853500b33d9ca396c2ad706a772f4b8
votmo <votmoyapochta1@bk.ru>
2023-05-03 21:19:18 +0300

    Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210 #413938]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,29 +1,22 @@
-package ru.job4j;
+package ru.job4j.grabber;
 
 import org.jsoup.Jsoup;
 import org.jsoup.Connection;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.utils.HabrCareerDateTimeParser;
+import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
-
+    private static  final String PAGE_LINK = "%s/vacancies/java_developer?page=";
     public static void main(String[] args) throws IOException {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-        List<String> listPages = new ArrayList<>();
-        listPages.add(String.format("%s/vacancies/java_developer?page=1", SOURCE_LINK));
-        listPages.add(String.format("%s/vacancies/java_developer?page=2", SOURCE_LINK));
-        listPages.add(String.format("%s/vacancies/java_developer?page=3", SOURCE_LINK));
-        listPages.add(String.format("%s/vacancies/java_developer?page=4", SOURCE_LINK));
-        listPages.add(String.format("%s/vacancies/java_developer?page=5", SOURCE_LINK));
-        for (String page : listPages) {
-            Connection connection = Jsoup.connect(page);
+        for (int i = 0; i < 5; i++) {
+            Connection connection =
+                    Jsoup.connect(String.format(PAGE_LINK.concat(String.valueOf(i + 1)), SOURCE_LINK));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
