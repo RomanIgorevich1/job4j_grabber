ea1465d5db9b0e189c42e8473bc7d179afd8bba9
votmo <votmoyapochta1@bk.ru>
2023-05-04 11:49:16 +0300

    Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -11,23 +11,45 @@ import java.io.IOException;
 
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "https://career.habr.com";
-    private static  final String PAGE_LINK = "%s/vacancies/java_developer?page=%d";
+    private static final String PAGE_LINK = "%s/vacancies/java_developer?page=%d";
+
     public static void main(String[] args) throws IOException {
         HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
-            for (int i = 1; i < 5; i++) {
-                Connection connection =
-                        Jsoup.connect(PAGE_LINK.formatted(SOURCE_LINK, i));
-                Document document = connection.get();
-                Elements rows = document.select(".vacancy-card__inner");
-                rows.forEach(row -> {
-                    Element titleElement = row.select(".vacancy-card__title").first();
-                    Element dataElement = row.select(".vacancy-card__date").first().child(0);
-                    Element linkElement = titleElement.child(0);
-                    String vacancyName = titleElement.text();
-                    String link = String.format("%s%s%n %s", SOURCE_LINK, linkElement.attr("href"),
-                            parser.parse(dataElement.attr("datetime")));
-                    System.out.printf("%s %s%n", vacancyName, link);
-                });
-            }
+        HabrCareerParse careerParse = new HabrCareerParse();
+        for (int i = 1; i < 2; i++) {
+            Connection connection =
+                    Jsoup.connect(PAGE_LINK.formatted(SOURCE_LINK, i));
+            Document document = connection.get();
+            Elements rows = document.select(".vacancy-card__inner");
+            rows.forEach(row -> {
+                Element titleElement = row.select(".vacancy-card__title").first();
+                Element dataElement = row.select(".vacancy-card__date").first().child(0);
+                Element linkElement = titleElement.child(0);
+                String vacancyName = titleElement.text();
+                String link = String.format("%s%s%n %s", SOURCE_LINK, linkElement.attr("href"),
+                        parser.parse(dataElement.attr("datetime")));
+                System.out.printf("%s %s%n %s%n", vacancyName, link,
+                        careerParse.retrieveDescription(linkElement.attr("href")));
+            });
+
+        }
+    }
+    /**
+     * Загрузка описания объявления
+     * connection.get().select(".vacancy-description__text").text()
+     * @param link ссылка вакансии
+     * @return само описание
+     */
+    private String retrieveDescription(String link) {
+        String description = "";
+        try {
+            Connection connection = Jsoup.connect(String.format("%s%s", SOURCE_LINK, link));
+            Document document = connection.get();
+            Elements row = document.select(".vacancy-description__text");
+            description = row.text();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+        return description;
     }
 }
